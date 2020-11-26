f351bc76384a473e45ed43488c0efcfea7fa16d2
daniela <danielaocampoo@gmail.com>
2020-11-25 22:41:19 -0500

    admin en private

--- src/app/components/private-tasks/private-tasks.component.ts
+++ src/app/components/private-tasks/private-tasks.component.ts
@@ -16,11 +16,14 @@ export class PrivateTasksComponent implements OnInit {
     this.tasksService.getPrivateTasks()
       .subscribe(
         res=>{
-          //console.log(res);
-          this.tasks=res;
+          console.log("prueba2");
+          console.log(res.users[0].name);
+          console.log(res, "entro a private task");
+          this.tasks=res.users;
         },
         err=>console.log("error") //err
       )
+
   }
 
 }
